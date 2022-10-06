2a3007f6737b44658629ad61d8171b5d57a42f1a
rickybarbour <rickybarbour@gmail.com>
2022-10-05 23:13:40 -0500

    Update backend config as needed

--- backends.tf
+++ backends.tf
@@ -1,8 +1,22 @@
 provider "azurerm" {
-  features {}
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+      purge_soft_deleted_keys_on_destroy = true
+      purge_soft_deleted_secrets_on_destroy = true
+      recover_soft_deleted_key_vaults = true
+      recover_soft_deleted_secrets = true
+    }       
+  }
 }
 
 terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.25.0"
+    }
+  }    
   backend "azurerm" {
     resource_group_name  = "rg-devopsprimer-shared-01"
     storage_account_name = "sttfdevopsprimer01"
