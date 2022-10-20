fc5d80347ae7301d8cec4440f945f8246acced4f
rickybarbour <rickybarbour@gmail.com>
2022-10-19 22:16:42 -0500

    Added providers and features

--- providers.tf
+++ providers.tf
@@ -14,7 +14,15 @@ terraform {
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+      purge_soft_deleted_keys_on_destroy = true
+      purge_soft_deleted_secrets_on_destroy = true
+      recover_soft_deleted_key_vaults = true
+      recover_soft_deleted_secrets = true
+    }       
+  }
 }
 
 provider "random" {}
